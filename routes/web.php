36b11833742ae17d24f739f4a8fc210547992d35
Nabil-Fauzan <24_nabilfauzan@student.smkti.net>
2026-07-24 18:12:38 +0700

    fix: add type hint for authenticated user to resolve static analysis lint error

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,7 @@ Route::get('/', function () {
 });
 
 Route::get('/dashboard', function () {
+    /** @var \App\Models\User $user */
     $user = Auth::user();
     $today = Carbon::today()->toDateString();
 
