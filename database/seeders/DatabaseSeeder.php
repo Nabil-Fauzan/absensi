3e0c55568b78f1c69d946cbf623d85008d75a2d8
Nabil-Fauzan <24_nabilfauzan@student.smkti.net>
2026-07-25 17:26:16 +0700

    chore: update DatabaseSeeder to seed user@gmail.com and admin@gmail.com by default

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,23 +3,30 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
-    use WithoutModelEvents;
-
     /**
      * Seed the application's database.
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
+        // Admin Account
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Administrator',
+            'email' => 'admin@gmail.com',
+            'password' => Hash::make('123456789'),
+            'role' => 'admin',
+        ]);
+
+        // Regular Employee Account
+        User::factory()->create([
+            'name' => 'Regular Employee',
+            'email' => 'user@gmail.com',
+            'password' => Hash::make('123456789'),
+            'role' => 'employee',
         ]);
     }
 }
